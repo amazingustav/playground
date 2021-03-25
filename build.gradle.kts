05eb6454ba82d6a5333623ef16ad1d243aa714d1
amazingustav <gustavoamorim02@gmail.com>
2021-03-25 11:29:15 -0300

    First commit with Ktor backend template

---
+++ build.gradle.kts
@@ -0,0 +1,37 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    kotlin("jvm") version "1.4.31"
+    application
+}
+
+group = "br.com.amz"
+version = "1.0"
+
+repositories {
+    jcenter()
+    mavenCentral()
+    maven { url = uri("https://dl.bintray.com/kotlin/kotlinx") }
+    maven { url = uri("https://dl.bintray.com/kotlin/ktor") }
+}
+
+dependencies {
+    testImplementation(kotlin("test-junit5"))
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.6.0")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.6.0")
+    implementation("io.ktor:ktor-server-netty:1.4.0")
+    implementation("io.ktor:ktor-html-builder:1.4.0")
+    implementation("org.jetbrains.kotlinx:kotlinx-html-jvm:0.7.2")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+tasks.withType<KotlinCompile>() {
+    kotlinOptions.jvmTarget = "13"
+}
+
+application {
+    mainClass.set("br.com.amz.playground.application.ServerKt")
+}
