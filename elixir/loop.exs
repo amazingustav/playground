3aad09bf6173db3fc56024ef2173b31cbda4a233
amazingustav <gustavoamorim02@gmail.com>
2022-04-04 01:02:23 -0300

    clauses with the same name should be grouped together

--- elixir/loop.exs
+++ elixir/loop.exs
@@ -1,10 +1,9 @@
 defmodule MyModule.Loop do
-    defp multiplication_table(_, 11), do: nil
-
     def multiplication_table(multiplier) do
         multiplication_table(multiplier, 1)
     end
 
+    defp multiplication_table(_, 11), do: nil
     defp multiplication_table(product1, product2) do
         IO.puts("#{product1} x #{product2} = #{product1 * product2}")
         multiplication_table(product1, product2 + 1)
