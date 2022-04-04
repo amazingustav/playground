750a09f364a160327e73ab2162f511058a0cf094
amazingustav <gustavoamorim02@gmail.com>
2022-04-04 00:22:01 -0300

    Learning aliases

--- elixir/play.exs
+++ elixir/play.exs
@@ -2,8 +2,10 @@ defmodule MyModule do
     import IO, only: [puts: 1]
     import Kernel, except: [inspect: 1]
 
+    alias MyModule.Math, as: MyMath
+
     def hello_world do
-        inspect(MyModule.Math.sum(2, 2))
+        inspect(MyMath.sum(2, 2))
     end
 
     def inspect(param) do
